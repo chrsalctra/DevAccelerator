087e64004f24bcc0e88dc73326aecbb17e887591
Christian Elijah Alcantara <chrsalctra@icloud.com>
2023-12-22 17:07:33 -0500

    Updates

---
+++ CA_java_udemy/Casting.java
@@ -0,0 +1,9 @@
+package CA_java_udemy;
+
+public class Casting {
+    public static void main(String[] args) {
+        int the = 5;
+        String six_ = "5";
+        System.out.println(Integer.parseInt(six_));
+    }
+}
---
+++ CA_java_udemy/TestFloat.java
@@ -0,0 +1,13 @@
+package CA_java_udemy;
+
+public class TestFloat{
+public static void main(String[] args){
+    int number1= 10.128952248;
+    float number2 = 10.128952248f;
+    double number3 = 10.128952248d;
+ 
+    System.out.println(number1);
+    System.out.println(number2);
+    System.out.println(number3);
+    }
+}
