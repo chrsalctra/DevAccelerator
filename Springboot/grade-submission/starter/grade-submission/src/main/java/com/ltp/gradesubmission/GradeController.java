80cededca3537e9be0c1a6fc43e67571d562baa8
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-02-25 22:24:21 -0500

    Grade submission

--- Springboot/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ Springboot/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -2,12 +2,27 @@ package com.ltp.gradesubmission;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.*;
+
 @Controller
 public class GradeController {
+
+    // List<Grade> studentGrades = new ArrayList<>(); --> causes loop generation issue (repeats)
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Harry", "Potions", "C-"),
+        new Grade("Chris", "Arithmacy", "A-"),
+        new Grade("Harry", "Charms", "B+")
+    );
+    @GetMapping("/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Harry", "Potions","C-" );
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
+        // Grade grade = new Grade("Nevil", "Potions","C-" );
+        //model.addAttribute("grade", grade);
         return "grades";
     }
 }
